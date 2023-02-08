63fe537fb1514a683f5496c52c8e59b2bbc222df
Mike Teuten <mike.teuten@gmail.com>
2023-02-08 18:57:03 +0000

    Removed all cards now redundant and associated js.

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,16 @@
-let appi = "https//:api.edamam.com/api/food-database/v2/parser"
-let appId = "0476dd24"
-let appiKey = "8c218a390102fe4aaef56c233b1a1c53"
+// Edamam data.
 
+let edappi = "https//:api.edamam.com/api/food-database/v2/parser"
+let edappId = "0476dd24"
+let edappiKey = "8c218a390102fe4aaef56c233b1a1c53"
+
+// Mealdb data.
+var Meal1 = "www.themealdb.com/api/json/v1/1/random.php"
+
+
+fetch('{Meal1}q=meals')
+.then(response => response.json())
+.then(meals => {
+    
+    console.log(meals[3])
+})
